7c8af0600d973d1ca48e85a0226fc5c21d6c71d6
Jess <jsscthomp@gmail.com>
2017-11-03 12:31:53 -0400

    dividers

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
     $('.parallax').parallax();
 
     $('.scrollspy').scrollSpy();
+
+    var options = [
+        {selector: '#staggered-test', offset: 400, callback: function(el) {
+            Materialize.showStaggeredList($(el));
+        } },
+      ];
+      Materialize.scrollFire(options);
 })
